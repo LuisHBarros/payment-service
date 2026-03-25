2be44bc5249c2144b158126ca10a54402ab6d175
luishbarros <luishrbr@outlook.com>
2026-03-25 19:54:00 -0300

    feat: add spring-retry dependency and enable @Retryable support
    - Add spring-retry dependency to pom.xml
    - Add @EnableRetry annotation to PaymentServiceApplication

--- src/main/java/com/payment/payment_service/PaymentServiceApplication.java
+++ src/main/java/com/payment/payment_service/PaymentServiceApplication.java
@@ -2,8 +2,10 @@ package com.payment.payment_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.retry.annotation.EnableRetry;
 
 @SpringBootApplication
+@EnableRetry
 public class PaymentServiceApplication {
 
 	public static void main(String[] args) {

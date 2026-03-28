ea932a84f724f9973a49af9554ef1139e6a0f25c
luishbarros <luishrbr@outlook.com>
2026-03-28 14:27:01 -0300

    refactor: remove obsolete transfer classes (TransactionController, listeners, exception)

--- src/main/java/com/payment/payment_service/transfer/exception/InvalidTransferException.java
+++
@@ -1,8 +0,0 @@
-package com.payment.payment_service.transfer.exception;
-
-public class InvalidTransferException extends RuntimeException {
-    
-    public InvalidTransferException(String message) {
-        super(message);
-    }
-}
